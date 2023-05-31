1358e275e4a318f48a22401426c7cd68daaeb283
Vu Hoang Giang <katana2812@gmail.com>
2023-05-31 10:14:29 +0700

    add scrapy

--- nc/backend/news_scrapy/news_scrapy/spiders/vnexpress.py
+++ nc/backend/news_scrapy/news_scrapy/spiders/vnexpress.py
@@ -1,5 +1,6 @@
 import scrapy
-
+from news_scrapy.settings import VNEXPRESS_SELECTORS
+from news_scrapy.items import ArticleItem
 
 class VnexpressSpider(scrapy.Spider):
     name = "vnexpress"
@@ -7,4 +8,14 @@ class VnexpressSpider(scrapy.Spider):
     start_urls = ["http://vnexpress.net/"]
 
     def parse(self, response):
-        pass
+        for article in response.css(VNEXPRESS_SELECTORS['article']).getall():
+            yield response.follow(article, self.parse_article)
+
+    def parse_article(self, response):
+        item = ArticleItem()
+        item.title = response.css(VNEXPRESS_SELECTORS['title']).get()
+        item.url = response.url
+        item.content = response.css(VNEXPRESS_SELECTORS['content']).get()
+        item.source = "vnexpress.net"
+        item.published_date = response.css(VNEXPRESS_SELECTORS['publication_date']).get()
+        yield item
