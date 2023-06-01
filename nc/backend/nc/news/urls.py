756302641b460500f64172dca2c0ee2d8e311018
Vu Hoang Giang <katana2812@gmail.com>
2023-06-01 14:08:08 +0700

    add an api to list all categories and order by name

--- nc/backend/nc/news/urls.py
+++ nc/backend/nc/news/urls.py
@@ -1,7 +1,8 @@
-from .views import ArticleList, ArticleDetail
+from .views import ArticleList, ArticleDetail, CategoryList
 from django.urls import path
 
 urlpatterns = [
     path("articles", ArticleList.as_view(), name="news.articles"),
     path("articles/<int:pk>/", ArticleDetail.as_view(), name="article_detail"),
+    path("categories/", CategoryList.as_view(), name="categories"),
 ]
--- nc/backend/nc/news/views.py
+++ nc/backend/nc/news/views.py
@@ -1,12 +1,12 @@
 from rest_framework import generics
-from .models import Article
-from .serializers import ArticleSerializer
+from .models import Article, Category
+from .serializers import ArticleSerializer, CategorySerializer
 from rest_framework.filters import SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
 
 
 class ArticleList(generics.ListAPIView):
-    queryset = Article.objects.all().order_by('-published_date')
+    queryset = Article.objects.all().order_by("-published_date")
     serializer_class = ArticleSerializer
     filter_backends = [SearchFilter, DjangoFilterBackend]
     search_fields = ["category__id"]
@@ -16,3 +16,8 @@ class ArticleList(generics.ListAPIView):
 class ArticleDetail(generics.RetrieveAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+
+
+class CategoryList(generics.ListAPIView):
+    queryset = Category.objects.all().order_by("name")  # Order by name
+    serializer_class = CategorySerializer
